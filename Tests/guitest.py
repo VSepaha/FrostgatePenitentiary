8a6f50f5b24937818a986e71b9b6e4140f177e1d
TKhare <tjskhare@gmail.com>
2017-06-27 19:21:45 -0400

    GUI Counter Options working

--- Tests/guitest.py
+++ Tests/guitest.py
@@ -2,7 +2,7 @@ import pygame
 
 STAMINA = 0
 HEALTH = 1
-
+RAMEN = 2
 
 pygame.init()
 
@@ -10,16 +10,22 @@ class GUI:
     def __init__(self):
         self.health = 100
         self.stamina = 100
+        self.ramen = 100
 
     def decrease_stat(self, stat):
         if stat == HEALTH:
             if self.health <= 0:
                 return # Do nothing
             self.health -= 1
-        else:
+        elif stat == STAMINA:
             if self.stamina <= 0:
                 return
-            self.stamina -= 1
+            self.stamina -= 1  
+        else:
+            if self.ramen <= 0:
+                return
+            self.ramen -= 1
+
 
     def increase_stat(self, stat):
 
@@ -27,10 +33,14 @@ class GUI:
             if self.health >= 100:
                 return
             self.health += 1
-        else:
+        elif stat == STAMINA:
             if self.stamina >= 100:
                 return
-            self.stamina += 1
+            self.stamina += 1  
+        else:
+            if self.ramen >= 999:
+                return
+            self.ramen += 1
 
     def get_stat(self, stat):
         if stat == HEALTH:
@@ -48,13 +58,20 @@ class GUI:
     def staminad(self):
         #displays 'stamina'
         Font2 = pygame.font.SysFont('monaco', 24)
-        heatSurface = Font2.render('Stamina: {0}%'.format(self.stamina), True, green)
-        heatRect = heatSurface.get_rect()
-        heatRect.midtop = (192, 45)
-        DISPLAYSURF.blit(heatSurface,heatRect)
+        stamSurface = Font2.render('Stamina: {0}%'.format(self.stamina), True, green)
+        stamRect = stamSurface.get_rect()
+        stamRect.midtop = (192, 45)
+        DISPLAYSURF.blit(stamSurface,stamRect)
+    def ramened(self):
+        #displays ': (ramen amount)'
+        Font3 = pygame.font.SysFont('monaco', 44)
+        ramSurface = Font3.render(': {0}'.format(self.ramen), True, black)
+        ramRect = ramSurface.get_rect()
+        ramRect.midtop = (940, 23)
+        DISPLAYSURF.blit(ramSurface,ramRect)
 
 
-w, h = 900, 550
+w, h = 1000, 600
 DISPLAYSURF = pygame.display.set_mode((w, h))
 
 GUI_display = GUI()
@@ -64,12 +81,12 @@ ramen = pygame.image.load('ramen.png')
 #colors
 red = (255, 0, 0)
 green = (0, 255, 0)
-
+black = (0, 0, 0)
 
 
 while True:
-    DISPLAYSURF.fill((0, 0, 0))
-    DISPLAYSURF.blit(ramen, (10,100))
+    DISPLAYSURF.fill((255, 255, 255))
+    DISPLAYSURF.blit(ramen, (830,13))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -90,6 +107,11 @@ while True:
     if key_pressed[pygame.K_c]:
         GUI_display.increase_stat(STAMINA)
 
+    if key_pressed[pygame.K_n]:
+        GUI_display.decrease_stat(RAMEN)
+    if key_pressed[pygame.K_b]:
+        GUI_display.increase_stat(RAMEN)
+
 
     # *2.55 because it ranges from 0-100, and 255 is color max
     health = GUI_display.get_stat(HEALTH)
@@ -104,5 +126,6 @@ while True:
  
     GUI_display.healthy()
     GUI_display.staminad()
+    GUI_display.ramened()
     pygame.display.flip()
 
