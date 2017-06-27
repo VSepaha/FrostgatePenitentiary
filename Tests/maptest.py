4befd945d30141ab3a59abc8f892594c7435aed8
VSepaha <vineetsepaha@gmail.com>
2017-06-27 19:20:18 -0400

    Basic tilemap works

--- Tests/maptest.py
+++ Tests/maptest.py
@@ -3,23 +3,9 @@ from pygame.locals import *
 
 pygame.init()
 
-TILESIZE = 40
-MAPWIDTH = 25
-MAPHEIGHT = 15
 
-
-class Map:
-	def __init__(self):
-
-		# Textures on the map 
-		self.textures = {
-			'R' :  pygame.image.load('../Resources/ground.png'),
-			'G' :  pygame.image.load('../Resources/grass.png'),
-			'W' :  pygame.image.load('../Resources/water.png'), 
-			'D' :  pygame.image.load('../Resources/dirt.png')
-		}
-
-# FPS Setting
+########## Settings
+# FPS 
 FPS = 30
 
 # Colors
@@ -36,21 +22,52 @@ GRASS = 'G'
 WATER = 'W'
 DIRT = 'D'
 
+# Map Values
+TILESIZE = 40
+MAPWIDTH = 25
+MAPHEIGHT = 15
+
+#######################
+
+
+class Map:
+	def __init__(self):
+
+		# Textures on the map 
+		self.textures = {
+			'R' :  pygame.image.load('../Resources/ground.png'),
+			'G' :  pygame.image.load('../Resources/grass.png'),
+			'W' :  pygame.image.load('../Resources/water.png'), 
+			'D' :  pygame.image.load('../Resources/dirt.png')
+		}
+		# open the map file
+		self.file = open('map.txt', 'r')
+
+		# set the display size of window
+		self.WIDTH = TILESIZE * MAPWIDTH
+		self.HEIGHT = TILESIZE * MAPHEIGHT
+
+
+	def createMap(self, DISPLAYSURF):
+
+		for i in range (0, MAPHEIGHT):
+			for j in range (0, MAPWIDTH):
+				return_char = self.file.read(1)
+				if return_char == '\n' or return_char == '':
+					return_char = self.file.read(1)
+				DISPLAYSURF.blit(game_map.textures[return_char],(j*TILESIZE,i*TILESIZE))
+
 # Instantiate Map
 game_map = Map()
-WIDTH = TILESIZE * MAPWIDTH
-HEIGHT = TILESIZE * MAPHEIGHT
 
 # Used to ensure a maximum fps setting
 fps_clock = pygame.time.Clock()
 
 # Set up the window and caption
-DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+DISPLAYSURF = pygame.display.set_mode((game_map.WIDTH, game_map.HEIGHT), 0, 32)
 DISPLAYSURF.fill(WHITE)
 
-for i in range (0,MAPWIDTH):
-	for j in range (0,MAPHEIGHT):
-		DISPLAYSURF.blit(game_map.textures[WATER],(i*TILESIZE,j*TILESIZE))
+game_map.createMap(DISPLAYSURF)
 
 
 # Main game loop
