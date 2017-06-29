f2dd49007ecb72eedaf4474a018d14084097f2aa
VSepaha <vineetsepaha@gmail.com>
2017-06-29 17:46:24 -0400

    implemented player and guard in test.py

--- Tests/test.py
+++ Tests/test.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from pygame.locals import *
+from PrisonMap import *
 
 pygame.init()
 
@@ -34,38 +35,173 @@ STAMINA = 0
 HEALTH = 1
 RAMEN = 2
 
-class Map:
-	def __init__(self):
+# Directions
+UP = 0
+DOWN = 1
+LEFT = 2
+RIGHT = 3
 
-		# Textures on the map 
-		self.textures = {
-			'R' :  pygame.image.load('../Resources/Tiles/ground.png'),
-			'G' :  pygame.image.load('../Resources/Tiles/grass.png'),
-			'W' :  pygame.image.load('../Resources/Tiles/water.png'), 
-			'D' :  pygame.image.load('../Resources/Tiles/dirt.png')
+# Actor types
+PLAYER = "player"
+PRISON_GUARD = "guard"
+
+# States that the guard can be in 
+PATROL = 0 
+STAND = 1
+CHASE = 2
+
+#######################
+
+# This is the actor class that all people will inherit from
+class Actor(pygame.sprite.Sprite):
+	def __init__(self, offset_x, offset_y, actor_type):
+		# Call the parent class (Sprite) constructor
+		pygame.sprite.Sprite.__init__(self)
+
+		directory = '../Resources/' + actor_type + '/' + actor_type
+
+		# Set up all of the images for any actor
+		self.images_front = []
+		self.images_front.append(pygame.image.load(directory + '_front_0.png'))
+		self.images_front.append(pygame.image.load(directory + '_front_1.png'))
+		self.images_front.append(pygame.image.load(directory + '_front_2.png'))
+
+		self.images_back = []
+		self.images_back.append(pygame.image.load(directory + '_back_0.png'))
+		self.images_back.append(pygame.image.load(directory + '_back_1.png'))
+		self.images_back.append(pygame.image.load(directory + '_back_2.png'))
+
+		self.images_left = []
+		self.images_left.append(pygame.image.load(directory + '_left_0.png'))
+		self.images_left.append(pygame.image.load(directory + '_left_1.png'))
+		self.images_left.append(pygame.image.load(directory + '_left_2.png'))
+
+		self.images_right = []
+		self.images_right.append(pygame.image.load(directory + '_right_0.png'))
+		self.images_right.append(pygame.image.load(directory + '_right_1.png'))
+		self.images_right.append(pygame.image.load(directory + '_right_2.png'))
+
+		# Mapping the direction with the variable
+		self.direction_map = {
+			UP : self.images_back,
+			DOWN: self.images_front,
+			LEFT : self.images_left,
+			RIGHT : self.images_right
 		}
 
-		# set the display size of window
-		self.WIDTH = TILESIZE * MAPWIDTH
-		self.HEIGHT = TILESIZE * MAPHEIGHT
+		# Set the speed of the player (change in distance)
+		self.speed = 4
 
-		self.tile_list = [None]*MAPHEIGHT*MAPWIDTH
+		# Set the current image
+		self.current_image = self.images_front[0]
 
-		file = open('map.txt', 'r')
-		for i in range (0, MAPHEIGHT*MAPWIDTH):
-			return_char = file.read(1)
-			if return_char == '\n' or return_char == '':
-				return_char = file.read(1)
-			self.tile_list[i] = return_char
-		file.close()
+		# Setting for the update function
+		self.change_timer = 6
+		self.index = 1
+		self.count = 0
+
+		# Fetch the rectangle object that has the dimensions of the image
+		# Update the position of this object by setting the values of rect.x and rect.y
+		self.rect = self.current_image.get_rect()
+		self.rect.x = offset_x
+		self.rect.y = offset_y
+
+	# Works Nicely
+	def update(self, move, direction):
+		if move == True:
+			self.count += 1
+			if self.count % self.change_timer == 0:
+				self.index += 1
+			if self.index >= len(self.direction_map[direction]):
+				self.index = 1
+			self.current_image = self.direction_map[direction][self.index]
+			if direction == UP:
+				self.move(0, -self.speed)
+			if direction == DOWN:
+				self.move(0, self.speed)
+			if direction == LEFT:
+				self.move(-self.speed, 0)
+			if direction == RIGHT:
+				self.move(self.speed, 0)
+  		else:
+  			self.index = 0
+  			self.count = 0
+  			self.current_image = self.direction_map[direction][self.index]
+
+  	def move(self, dx, dy):
+  		if dx != 0:
+  			self.move_single_axis(dx, 0)
+  		if dy != 0:
+  			self.move_single_axis(0, dy)
+
+  	def move_single_axis(self, dx, dy):
+  		# Move the rect
+  		self.rect.x += dx
+  		self.rect.y += dy
 
 
-	def display_map(self, DISPLAYSURF):
-		index = 0
-		for i in range (0, MAPHEIGHT):
-			for j in range (0, MAPWIDTH):
-				DISPLAYSURF.blit(game_map.textures[self.tile_list[index]],(j*TILESIZE,i*TILESIZE))
-				index += 1
+# This is the class that will be used by the NPCs
+class Guard(Actor):
+	def __init__(self, offset_x, offset_y, actor_type):
+		# Don't forget to call the parent class!
+		Actor.__init__(self, offset_x, offset_y, actor_type)
+
+		# Route set for the NPC
+		self.route = [
+			(offset_x, offset_y + 300),
+			(offset_x-500, offset_y + 300),
+			(offset_x-500, offset_y),
+			(offset_x, offset_y) 
+		]
+		self.route_index = 0
+
+		# To keep track of whether the NPC is moving
+		self.moving = False
+
+		# slowing down the speed for patrol
+		self.speed = 2
+		self.change_timer = 8
+
+
+	# Guard patrol route implementation
+	def start_patrol(self, state):
+		route_len = len(self.route)
+		if state == PATROL:
+			if self.rect.x != self.route[self.route_index][0] or self.rect.y != self.route[self.route_index][1]:
+				self.moving = True
+				if self.rect.x > self.route[self.route_index][0]:
+					self.update(True, LEFT)
+				elif self.rect.x < self.route[self.route_index][0]:
+					self.update(True, RIGHT)
+				if self.rect.y > self.route[self.route_index][1]:
+					self.update(True, UP)
+				elif self.rect.y < self.route[self.route_index][1]:
+					self.update(True, DOWN)
+				self.moving == True
+			else:
+				self.moving = False
+
+			if self.route_index < route_len-1 and self.moving == False:
+				self.route_index += 1
+				self.update(False, DOWN)
+			elif self.route_index >= route_len-1 and self.moving == False:
+				self.route_index = 0
+				self.update(False, DOWN)
+
+
+def key_down_events(event):
+	# Nothing for now
+	print "Key down:", event.key
+
+def key_up_events(event):
+	if event.key == K_w:
+		player.update(False, UP)
+	if event.key == K_s:
+		player.update(False, DOWN)
+	if event.key == K_a:
+		player.update(False, LEFT)
+	if event.key == K_d:
+		player.update(False, RIGHT)
 
 class GUI:
     def __init__(self):
@@ -139,8 +275,10 @@ class GUI:
         DISPLAYSURF.blit(self.ramen_image, (830,13))
 
 
-game_map = Map()
+game_map = PrisonMap()
 GUI_display = GUI()
+player = Actor(100, 100, PLAYER)
+guard = Guard(800, 20, PRISON_GUARD)
 
 # Used to ensure a maximum fps setting
 fps_clock = pygame.time.Clock()
@@ -153,13 +291,33 @@ while True:
 	game_map.display_map(DISPLAYSURF)
 
 	for event in pygame.event.get():
-		if event.type == QUIT:
+		if event.type == KEYDOWN:
+			key_down_events(event)
+		if event.type == KEYUP:
+			key_up_events(event)
+		elif event.type == QUIT:
 			pygame.quit()
 			sys.exit()
+
+	key_pressed = pygame.key.get_pressed()
+
+	if key_pressed[K_w]: 
+		player.update(True, UP)
+	elif key_pressed[K_s]:
+		player.update(True, DOWN)
+	elif key_pressed[K_a]:
+		player.update(True, LEFT)
+	elif key_pressed[K_d]:
+		player.update(True, RIGHT)
 
 	GUI_display.display_health()
 	GUI_display.display_stamina()
 	GUI_display.display_ramen()
+
+	guard.start_patrol(PATROL)
+
+	DISPLAYSURF.blit(guard.current_image, (guard.rect.x, guard.rect.y))
+	DISPLAYSURF.blit(player.current_image, (player.rect.x, player.rect.y))
 
 	pygame.display.update()
 	fps_clock.tick(FPS)
