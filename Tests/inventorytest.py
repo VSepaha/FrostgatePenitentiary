5b2071a72bcc9c465341a51ed865cc6726ca9ee1
VSepaha <vineetsepaha@gmail.com>
2017-06-29 23:45:50 -0400

    test updated

--- Tests/inventorytest.py
+++ Tests/inventorytest.py
@@ -2,27 +2,71 @@
 import pygame, sys
 pygame.init()
 
-i= 1
+an_item = 1
+baton = 2
+bat = 3
+key = 4
+pos0 = 0
+pos1 = 0
+pos2 = 0
+pos3 = 0
+pos4 = 0
 
-inv = []
+inv = [None, None, None, 'an_item', None]
 #Code below will add an item to that list.
 w, h = 1000, 600
+inventory = pygame.image.load('../Resources/inventory.png')
 DISPLAYSURF = pygame.display.set_mode((w, h))
+DISPLAYSURF.fill((255, 255, 255))
+
 while True:
-        DISPLAYSURF.fill((255, 255, 255))
+        DISPLAYSURF.blit(inventory, (400, 500))
+        if pos0 == 1:
+                pos0image = pygame.image.load('../Resources/an_item.png')
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         pygame.quit()
                         sys.exit()
                 elif event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_x:
-                                if len(inv) < 6:
-                                        inv.append("item")
+                                if inv[0] == None:
+                                        inv[0] = 'an_item'
+                                        pos0 = 1
+
+                                elif inv[1] == None:
+                                        inv[1] = 'an_item'
+                                        pos1 = 1
+                                elif inv[2] == None:
+                                        inv[2] = 'an_item'
+                                        pos2 = 1
+                                elif inv[3] == None:
+                                        inv[3] = 'an_item'
+                                        pos3 = 1
+                                elif inv[4] == None:
+                                        inv[4] = 'an_item'
+                                        pos4 = 1n
                                 else:
                                         print("No inventory space")
                         if event.key == pygame.K_z:
-                                inv.pop()
-                                print('Pop Successful')
+                                if inv[4] == None:
+                                        if inv[3] == None:
+                                                if inv[2] == None:
+                                                        if inv[1] == None:
+                                                                if inv[0] == None:
+                                                                        print('Nothing to drop!')
+                                                                else: inv[0] = None
+
+                                                        else:
+                                                                inv[1] = None
+                                                else:
+                                                        inv[2] = None
+                                        else:
+                                                inv[3] = None
+                                else:
+                                        inv[4] = None
                         if event.key == pygame.K_c:
                                 print(inv)
+                        if event.key == pygame.K_f:
+                                DISPLAYSURF.blit(pos0image, (400, 500))
+
 
