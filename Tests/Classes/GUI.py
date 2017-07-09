0473b8549f3d709baf04a154989eec110d85d3c8
TKhare <tjskhare@gmail.com>
2017-07-09 02:00:08 -0400

    map finished

--- Tests/Classes/GUI.py
+++ Tests/Classes/GUI.py
@@ -12,6 +12,7 @@ class GUI:
         self.ramen_image = pygame.image.load('ramen.png')
         self.inventory_image = pygame.image.load('../Resources/inventory.png')
         self.health_stamina_frame = pygame.image.load('../Resources/health_stamina_frame.png')
+        self.ramen_frame = pygame.image.load('../Resources/ramen_frame.png')
         self.inv = [nothing_object, nothing_object, nothing_object, nothing_object, nothing_object]
 
 
@@ -89,8 +90,9 @@ class GUI:
         ramSurface = Font3.render(': {0}'.format(self.ramen), True, BLACK)
         ramRect = ramSurface.get_rect()
         ramRect.midtop = (940, 23)
+        DISPLAYSURF.blit(self.ramen_frame, (830,4))
         DISPLAYSURF.blit(ramSurface,ramRect)
-        DISPLAYSURF.blit(self.ramen_image, (830,13))
+
 
         # Displays the inventory
         DISPLAYSURF.blit(self.inventory_image, (402, 502))
